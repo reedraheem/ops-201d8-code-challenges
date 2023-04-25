1fbf668050ee4e575774430ea2cc96c8ddb13430
reedraheem <reedsharif@yahoo.com>
2023-04-25 23:14:03 +0000

    script is about helloworld

---
+++ helloworld.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script Name: helloworld                 Script Name
+# Author:   rsr                   Your Name
+# Date of latest revision:      04/25/2023
+# Purpose: Create a script that prints a string to the console.                     Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+# found instructions here: https://www.cyberciti.biz/faq/hello-world-bash-shell-script/
+echo "Hello World"
+
+
+
+
+
+# End
