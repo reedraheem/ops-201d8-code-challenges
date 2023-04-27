17cdabba236ccf5b6dad91ce8f0614a8e655a576
reedraheem <Reedsharif@yahoo.com>
2023-04-26 19:27:22 -0700

    code challenge 2

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+function sharif (){
+   lastlog
+   echo "this is the login history"
+}
+sharif
+sharif
+sharif
+# End
