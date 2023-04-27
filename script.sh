e86ffb51d89911033a7f560eb995ebf0d0e63e06
reedraheem <Reedsharif@yahoo.com>
2023-04-26 19:28:52 -0700

    code challenge 2

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Script Name:                  Script Name
-# Author:                       Your Name
-# Date of latest revision:      00/00/0000
-# Purpose:                      Purpose
+# Script Name:script.sh code challenge 2                  Script Name
+# Author:  rsr                     Your Name
+# Date of latest revision:      04/26/2023
+# Purpose: create resuable code                     Purpose
 
 # Declaration of variables
-
+#no variables
 # Declaration of functions
-
+#sharif; print login history
 
 # Main
 function sharif (){
