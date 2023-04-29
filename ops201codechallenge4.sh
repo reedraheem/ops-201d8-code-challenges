c10ded18a59841149ba2b4542f512907b81641bc
reedraheem <Reedsharif@yahoo.com>
2023-04-29 01:27:19 +0000

    code4

---
+++ ops201codechallenge4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script:   code challenge 4                    
+# Author: rsr                      
+# Date of latest revision: April 28,2023     
+# Purpose: write a script that:Displays running processes
+#Asks the user for a PID
+#Kills the process with that PID
+#Starts over at step 1 and continues until the user exits with ctrl c                    
+
+while [ condition ]
+do
+   ps -aux
+   command2
+   command3
+done
