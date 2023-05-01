08a0e437cc9e898452a8b699b10ae6718ea1ae42
reedraheem <Reedsharif@yahoo.com>
2023-05-01 22:47:10 +0000

    code 4.0

--- ops201codechallenge4.sh
+++ ops201codechallenge4.sh
@@ -8,10 +8,20 @@
 #Kills the process with that PID
 #Starts over at step 1 and continues until the user exits with ctrl c                    
 
-playvar=1
-while [ $playvar -le 10 ]
-do
-    echo $playvar
-    playvar=$(( $playvar +1))
-    sleep 0.5
-done
+#Reference: got this script from chatgpt
+
+# Display running processes
+echo "Running processes:"
+ps -e
+
+# Ask the user for a PID
+echo "Enter PID to kill:"
+read pid
+
+# Kill the process with the specified PID
+if [ -z "$pid" ]; then
+  echo "No PID entered. Exiting."
+  exit 1
+else
+  kill "$pid"
+fi
