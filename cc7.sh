2aff1bd9748d1d239dbff7b71de65c5649e482b2
reedraheem <Reedsharif@yahoo.com>
2023-05-03 17:24:39 +0000

    code7

--- cc7.sh
+++ cc7.sh
@@ -31,6 +31,8 @@ sudo lshw -short -C memory | awk '/System Memory/{print $1,$2,$3,$4,$5,$6,$7}'
 sudo lshw -c display | awk '/description|product|vendor|physical id|bus info|width|clock|capabilities|configuration|resources/{print}'
 
 
+
+
 # Run lshw command and store output in a variable
 network_info=$(sudo lshw -class network)
 
@@ -43,5 +45,25 @@ width=$(echo "$network_info" | grep -E "width" | cut -d ":" -f 2 | sed 's/^[ \t]
 clock=$(echo "$network_info" | grep -E "clock" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
 capabilities=$(echo "$network_info" | grep -E "capabilities" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
 configuration=$(echo "$network_info" | grep -E "configuration" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
-resources=$(echo "$network
+resources=$(echo "$network_info" | grep -E "resources" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
+logical_name=$(echo "$network_info" | grep -E "logical name" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
+version=$(echo "$network_info" | grep -E "version" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
+serial=$(echo "$network_info" | grep -E "serial" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
+size=$(echo "$network_info" | grep -E "size" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
+capacity=$(echo "$network_info" | grep -E "capacity" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
 
+# Print out the network adapter information
+echo "Description: $description"
+echo "Vendor: $vendor"
+echo "Physical ID: $physical_id"
+echo "Bus Info: $bus_info"
+echo "Width: $width"
+echo "Clock: $clock"
+echo "Capabilities: $capabilities"
+echo "Configuration: $configuration"
+echo "Resources: $resources"
+echo "Logical Name: $logical_name"
+echo "Version: $version"
+echo "Serial: $serial"
+echo "Size: $size"
+echo "Capacity: $capacity"
